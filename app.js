71ead60b877af1e37fd808f9ef35462b5482b013
Arthur-Couto <arthurdocouto@ufrj.br>
2025-04-18 20:32:14 -0300

    btn reiniciar

--- app.js
+++ app.js
@@ -21,19 +21,33 @@ function sortear() {
         }
 
         sorteados.push(numero);
-    }
+    };
 
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
-}
-
-function reiniciar() {
-
-    
-}
+    alterarStatusBotao();
+};
 
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    };
+};
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: Nenhum até agora</label>`;
+    alterarStatusBotao();
+}
